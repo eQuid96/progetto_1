f8267aea63ba745498d38c1d19953de8e75dd8dd
Pier Carlo <imparatopiercarlo@gmail.com>
2017-02-18 17:51:31 +0100

    Add files via upload

---
+++ header.h
@@ -0,0 +1,39 @@
+#define END_GAME 3 // STATO DI FINE GIOCO
+#define YEL   "\x1B[33m" 
+#define GRN   "\x1B[32m"
+#define RESET "\x1B[0m"   // Costanti per i colori dei printf.
+#define RED   "\x1B[31m"
+
+int field[40] = {1,2,3,4,5,6,7,8,9,10,
+                 1,2,3,4,5,6,7,8,9,10,    // cAMPO DA GIOCO 0-39 CARTE
+                 1,2,3,4,5,6,7,8,9,10,
+                 1,2,3,4,5,6,7,8,9,10};
+
+typedef struct giocatore{
+    char *name;
+    int point;             //Struttura per la gestione dei giocatori,nome_giocatore,puntenggio_giocatore,avversario per lo switch.
+    int opponent;
+}player;
+
+player player1 = {"Giocatore1",0,2};
+player player2 = {"Giocatore2",0,1};
+
+player *ptr;  // puntantore per la gestione dei player
+
+extern int ATT_PLAYER = 0;  // Variabile esterna per controllare il turno di ogni giocatore.
+
+
+// Funzioni gestionali
+void stampCamp(int[],int);
+int getRandom(int,int);
+int sameCard(int card1, int card2);
+void mescola(int array[], int len);
+void getPlayerName(player *ptr);
+int getPlayerPoint(player *ptr);
+void addPlayerPoint(player *ptr);
+void getWinner(player *ptr1, player *ptr2);
+void playGame(int player);
+void scopriCarte(int card1, int card2);
+int isEndGame(player *ptr1,player *ptr2);
+void setName(char *player_name,player *ptr);
+// End Funzioni.
---
+++ main.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+#include <time.h>
+
+int main(){
+  int n;
+  char *name1,*name2;
+  srand((unsigned)time(NULL));
+  printf("Benvenuto,scegli quante volte mischiare il mazzo di carte: ");
+  scanf("%d",&n);
+  printf("Hai deciso di mischiare il mazzo %d volte.\n",n);
+  mescola(field,n);
+  printf("Giocatore1 scegli il tuo nome: ");
+  scanf("%s",&name1);
+  setName(&name1,&player1);
+  printf("Giocatore2 scegli il tuo nome: ");
+  scanf("%s",&name2);
+  setName(&name2,&player2);
+  printf("Viene scelto a caso il giocatore che inizierà a giocare . . .\n");
+  ATT_PLAYER = getRandom(1,2);
+  if(ATT_PLAYER == 2){
+    getPlayerName(&player2);
+    printf("Inizierà a giocare per primo\n");
+  }else{
+    getPlayerName(&player1);
+    printf("Inizierà a giocare per primo\n");
+  }
+  stampCamp(field,40);
+  playGame(ATT_PLAYER);
+  return 0;
+}
+
+
+void stampCamp(int array[], int len){
+  int i;
+  for(i=0;i<len;i++){
+    if (i == 9 || i == 19 || i == 29 || i == 39){
+      if(array[i]!=0){
+        printf(YEL" [] \n"RESET);
+      }else{
+        printf(GRN" ✔ \n"RESET);
+      }
+    }
+    else if(array[i] == 0){
+       printf(GRN" ✔ "RESET);
+    }else{
+      printf(YEL" [] "RESET);
+    }
+  }
+}
+
+
+int sameCard(int card1, int card2){
+  if(field[card1] != field[card2]){
+    return 0;
+  }else{
+    return 1;
+  }
+}
+
+void mescola(int array[], int len){
+  int i,card1,card2,temp;
+  for(i=0;i<len;i++){
+    card1 = (int)getRandom(0,39);
+    card2 = (int)getRandom(0,39);
+    temp = array[card2];
+    array[card2] = array[card1];
+    array[card1] = temp;
+   }
+}
+
+int getRandom(int a,int b){
+  int i;
+  i = (int) rand() % b + a;
+  return i;
+}
+
+void getPlayerName(player *ptr){
+  printf("[%s]: ",ptr->name);
+}
+
+int getPlayerPoint(player *ptr){
+  return ptr->point;
+}
+
+void addPlayerPoint(player *ptr){
+  int att_p = getPlayerPoint(ptr);
+  ptr->point = ++att_p;
+}
+
+void setName(char *player_name,player *ptr){
+  ptr->name = player_name;
+}
+
+void scopriCarte(int card1, int card2){
+  field[card1] = 0;
+  field[card2] = 0;
+}
+
+int isEndGame(player *ptr1,player *ptr2){
+  unsigned int sum;
+  sum = (unsigned int)ptr1->point + ptr2->point;
+  if (sum == 20){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+void getWinner(player *ptr1, player *ptr2){
+  int player1 = getPlayerPoint(ptr1);
+  int player2 = getPlayerPoint(ptr2);
+  if (player1 == player2){
+    printf(GRN"[Info]: La partita è finita in Preggio! (%s:%d - %s:%d)\n"RESET,ptr1->name,player1,ptr2->name,player2);
+    ATT_PLAYER = END_GAME;
+    return;
+  }
+  if(player1>player2){
+    printf("[Info]: La partita è finita!,Vince: %s! ("GRN"%s:%d"RESET" - %s:%d)\n",ptr1->name,ptr1->name,player1,ptr2->name,player2);
+    ATT_PLAYER = END_GAME;
+    return;
+  }
+  printf("[Info]: La partita è finita!,Vince: %s! ("GRN"%s:%d"RESET" - %s:%d)\n",ptr2->name,ptr2->name,player2,ptr1->name,player1);
+  ATT_PLAYER = END_GAME;
+  return;
+}
+
+
+void playGame(int player){
+  int card1,card2,isSame,isEnd;
+  // SWITCH DEL PUNTATORE DEL GIOCATORE
+  if(player == 1){
+    ptr = &player1;
+  }else{
+    ptr = &player2;
+  }
+    getPlayerName(ptr);
+    printf("Scegli le carte da scoprire,inserici il numero delle carte(0,39): ");
+    scanf("%d %d",&card1,&card2);
+  // CONTROLLI PER LA SICUREZZA DEL GIOCO
+   if (card1 == card2){
+      printf(RED"[Info]: Non puoi scegliere la stessa carta.\n"RESET);
+      playGame(ATT_PLAYER);
+    }
+    else if(field[card1] == 0 || field[card2] == 0){
+     printf(RED"[Info] Non puoi scegliere carte già scoperte.\n"RESET);
+     playGame(ATT_PLAYER);
+    }
+  // END CONTROLLI
+      getPlayerName(ptr);
+      isSame = sameCard(card1,card2);
+      if(!isSame)
+      {
+        printf(RED"Le carte non sono uguali tocca al prossimo giocatore..\n"RESET);
+        ATT_PLAYER = ptr->opponent;
+        playGame(ATT_PLAYER);
+      }
+      else
+      {
+        addPlayerPoint(ptr);
+        printf(GRN"Le carte sono uguali!,hai guadagnato un punto. Punteggio Giocatore: %d\n"RESET,getPlayerPoint(ptr));
+        scopriCarte(card1,card2);
+        stampCamp(field,40);
+        isEnd = isEndGame(&player1,&player2);
+        if(isEnd)
+        {
+          getWinner(&player1,&player2);
+          return;
+        }
+          playGame(ATT_PLAYER);
+        }
+}
